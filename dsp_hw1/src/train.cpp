fc18859b33b01d8fc44572db25d8a077a1d1f82c
eric42827 <b06502147@ntu.edu.tw>
2019-10-30 18:19:24 +0800

    alpha

--- dsp_hw1/src/train.cpp
+++ dsp_hw1/src/train.cpp
@@ -2,8 +2,12 @@
 #include "hmm.h"
 #include <math.h>
 #include <fstream>
-using namespace std;
+#define T_num 50 
+#define N_num 6 
 
+
+using namespace std;
+void  cal_alpha(string s,double**alpha );
 int main(int argc, char *argv[]){
     
     int iter = atoi(argv[1]);
@@ -14,15 +18,21 @@ int main(int argc, char *argv[]){
     //dumpHMM( stdout, &hmm_initial );
     ifstream fin;
     string seq;
+    double alpha[T_num][N_num];
+    double beta[T_num][N_num];
+
+
     fin.open(argv[3] , ios::in);
     if(fin){
-        while( getline(fin,seq) ){
-            cout << seq ;
+        while(getline(fin,seq) ){
+            
+            cout << seq[0] ;
             cout << '\n';
         }
         
     }
     fin.close();
+
 
 
 
@@ -63,4 +73,28 @@ int main(int argc, char *argv[]){
 //   }
 
     return 0;
+}
+
+double cal_alpha(string s,HMM h){
+    double alpha [h.observ_num][h.state_num];
+    for(int i = 0;i < h.observ_num;i++){//ocr num 50
+        for(int j = 0; j < h.state_num;j++){//state num  6
+            int ocu = s[i] - 'A';
+            if(i ==1){
+                alpha[i][j] = h.initial[j]*h.observation[ocu][j];
+            }
+            else if (i < h.observ_num-1)
+            {
+                for(int k = 0;k < h.state_num;k++){
+                    alpha[i][j] += (alpha[i-1][k]*h.transition[k][j]);
+                    alpha[i][j] *= 
+                }
+                
+            }
+            else{
+
+            }
+        }
+    }
+
 }
